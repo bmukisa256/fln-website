c8513bf47afc61bf1c01a98db12cf11358d84498
Aurits <alandaambrose@gmail.com>
2024-09-28 12:23:29 +0300

    send

--- messages.php
+++ messages.php
@@ -1,146 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FLN-FEMALE LAWYERS NETWORK</title>
+<section class="carosal-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="client owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="text">
+                            <h3>ABOUT US<i class="fa fa-users" aria-hidden="true"></i></h3>
+                            <p>The Female Lawyers’ Network (FLN) is a not-for-profit membership-based women in
+                                law network established in 2022. The FLN offers training and motivational
+                                sessions, empowerment sessions, mentorship and legal referral services with the
+                                aim to mentor and inspire women in law and girls.
+                                The FLN Membership cuts across sectors and age groups and encompasses members
+                                from the judiciary, private legal practice, in-house counsel, diplomatic
+                                mission, security forces, public service and law students.
+                                Further, the FLN membership comprises associate members who include the male
+                                Lawyers, male law students and any other people who embrace the FLN Agenda.</p>
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
-</head>
-
-<body>
-
-    <div class="wrapper">
-        <header class="header">
-            <section class="header-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <div class="contact">
-                                <p><span class="phone"><a href="#">Phone: +256 755999544</a></span><span
-                                        class="email"><a href="mailto:femalelawyersnetwork@gmail.com">Email:
-                                            femalelawyersnetwork@gmail.com</a></span></p>
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-sm-4">
-                            <div class="join-us">
-                                <p><a href="mailto:femalelawyersnetwork@gmail.com">FLN</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section class="header-bottom">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-5 col-sm-12 col-xs-12">
-                            <a href="#">
-                                <div class="main-logo">
-                                    <img src="imgs/FLN_logo-full_color_50.png" alt="FLN-logo">
-                                    <h2></h2>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-md-7 col-sm-12 col-xs-12">
-                            <div class="menu">
-                                <ul class="nav navbar-nav">
-                                    <li class="active"><a href="index.html">HOME</a></li>
-                                    <li class="active"><a href="flnpg1.html">MESSAGES</a></li>
-                                    <li class="active"><a href="flnpg2.html">VIRTUAL SESSIONS</a></li>
-                                    <li class="active"><a href="flnpg3.html">NEWS</a></li>
-                                    <li class="active"><a href="flnpg4.html">PICTORIAL</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </header>
-        <section class="carosal-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="client owl-carousel owl-theme">
-                            <div class="item">
-                                <div class="text">
-                                    <h3>ABOUT US<i class="fa fa-users" aria-hidden="true"></i></h3>
-                                    <p>The Female Lawyers’ Network (FLN) is a not-for-profit membership-based women in
-                                        law network established in 2022. The FLN offers training and motivational
-                                        sessions, empowerment sessions, mentorship and legal referral services with the
-                                        aim to mentor and inspire women in law and girls.
-                                        The FLN Membership cuts across sectors and age groups and encompasses members
-                                        from the judiciary, private legal practice, in-house counsel, diplomatic
-                                        mission, security forces, public service and law students.
-                                        Further, the FLN membership comprises associate members who include the male
-                                        Lawyers, male law students and any other people who embrace the FLN Agenda.</p>
-
-                                    <h5 class="white-button"><a href="mailto:femalelawyersnetwork@gmail.com">CONTACT
-                                            US</a></h5>
-                                </div>
-                            </div>
+                            <h5 class="white-button"><a href="mailto:femalelawyersnetwork@gmail.com">CONTACT
+                                    US</a></h5>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 
-        <section class="our_activity">
-            <h2>MESSAGE FROM PRESIDENT|FOUNDER</h2>
-            <p>"It is a delight to bring to you the good news from Female Lawyers’ Network (FLN) of an empowered Female
-                Lawyers and the girl child in a gender-equal and just world.
-                Female Lawyers’ Network is alive to the fact that gender equality is pivotal to the achievement of
-                sustainable development not only in Uganda but in the world at large and this is the very reason as to
-                why it comes to focus on core values of;<br>
-                • Gender equality<br>
-                • Good stewardship<br>
-                • Empowerment<br>
-                • Collaboration<br>
-                which values not only benefit female lawyers but also the girl child in general. Because times are
-                shifting and the world is evolving so fast, these core values allow women participate in decision making
-                and even shape the direction of any movement as and when it comes forth thus eradicating gender
-                inequalities, injustices and bias in all spaces worldwide.
-                Women are not any different from men. Once empowered, they actually perform greatly, save for the weak
-                background status driven by stereotypical cultural norms.<br>
-                At Female Lawyers’ Network, we strongly believe and trust that female lawyers and the girl child be
-                given a platform to make manifest of their greatness by involving them in the competitive atmospheres,
-                decision making and many more such avenues to promote good governance and uphold the Rule of Law. I
-                therefore urge us all to join the FLN noble cause to effectively walk the talk because we all are
-                stakeholders in a gender-equal and just world.
-                lift up a woman; Lift up society."<br>
-                Hon Dr.Joyce Nalunga<br>
-                President Female Lawyers’ Network (FLN)<br>
-            </p>
+<section class="our_activity">
+    <h2>MESSAGE FROM PRESIDENT|FOUNDER</h2>
+    <p>"It is a delight to bring to you the good news from Female Lawyers’ Network (FLN) of an empowered Female
+        Lawyers and the girl child in a gender-equal and just world.
+        Female Lawyers’ Network is alive to the fact that gender equality is pivotal to the achievement of
+        sustainable development not only in Uganda but in the world at large and this is the very reason as to
+        why it comes to focus on core values of;<br>
+        • Gender equality<br>
+        • Good stewardship<br>
+        • Empowerment<br>
+        • Collaboration<br>
+        which values not only benefit female lawyers but also the girl child in general. Because times are
+        shifting and the world is evolving so fast, these core values allow women participate in decision making
+        and even shape the direction of any movement as and when it comes forth thus eradicating gender
+        inequalities, injustices and bias in all spaces worldwide.
+        Women are not any different from men. Once empowered, they actually perform greatly, save for the weak
+        background status driven by stereotypical cultural norms.<br>
+        At Female Lawyers’ Network, we strongly believe and trust that female lawyers and the girl child be
+        given a platform to make manifest of their greatness by involving them in the competitive atmospheres,
+        decision making and many more such avenues to promote good governance and uphold the Rule of Law. I
+        therefore urge us all to join the FLN noble cause to effectively walk the talk because we all are
+        stakeholders in a gender-equal and just world.
+        lift up a woman; Lift up society."<br>
+        Hon Dr.Joyce Nalunga<br>
+        President Female Lawyers’ Network (FLN)<br>
+    </p>
 
-            <h2>MESSAGE FROM CHAIRPERSON</h2>
-            <p>"Female Lawyers Network.
-                <br>
-                To uphold women’s human rights is to ensure that every woman and girl lives up to her full potential.
-                This year we celebrated the International Women’s Day under the theme DigitALL: Innovation and
-                technology for gender equality”. We do not only celebrate the woman on this day but we also look to the
-                future and think about what we have yet to achieve.
-                The world today with the increase of technology there is a high likelihood that some people will be left
-                behind. That is why the Female Lawyers Network seeks to reach out to girls who intend to influence the
-                world through the law, so that they are not left behind
-                I invite us to search within you and ask ourselves how can I facilitate a young girl to achieve their
-                dream. I encourage you to cultivate a willingness within to look out for them, meet with them and invite
-                them to the Female Lawyers Network.
-                This way you will have contributed to a greater commitment of the Female Lawyers Network whose Mission
-                is to <br>"Eradicate Gender injustices through engagement and awareness with women and girls to promote
-                women rights, foster good governance and uphold Rule of Law in Africa"<br> and a Vision <br>"Empowered
-                female lawyers and the girl child in a gender equal and just world".<br>
-                I invite you all to subscribe to the Female Lawyers Network and make a difference in the life of a girl
-                child.
-                "
-            </p>
+    <h2>MESSAGE FROM CHAIRPERSON</h2>
+    <p>"Female Lawyers Network.
+        <br>
+        To uphold women’s human rights is to ensure that every woman and girl lives up to her full potential.
+        This year we celebrated the International Women’s Day under the theme DigitALL: Innovation and
+        technology for gender equality”. We do not only celebrate the woman on this day but we also look to the
+        future and think about what we have yet to achieve.
+        The world today with the increase of technology there is a high likelihood that some people will be left
+        behind. That is why the Female Lawyers Network seeks to reach out to girls who intend to influence the
+        world through the law, so that they are not left behind
+        I invite us to search within you and ask ourselves how can I facilitate a young girl to achieve their
+        dream. I encourage you to cultivate a willingness within to look out for them, meet with them and invite
+        them to the Female Lawyers Network.
+        This way you will have contributed to a greater commitment of the Female Lawyers Network whose Mission
+        is to <br>"Eradicate Gender injustices through engagement and awareness with women and girls to promote
+        women rights, foster good governance and uphold Rule of Law in Africa"<br> and a Vision <br>"Empowered
+        female lawyers and the girl child in a gender equal and just world".<br>
+        I invite you all to subscribe to the Female Lawyers Network and make a difference in the life of a girl
+        child.
+        "
+    </p>
 
-        </section>
-        <?php include('footer.php'); ?>
+</section>
+<?php include('footer.php'); ?>
