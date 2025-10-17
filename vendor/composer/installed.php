e4cc8d70b1253591bc3191169c06842e40432f5d
Aurits <alandaambrose@gmail.com>
2025-10-17 08:30:38 +0300

    Refactor code structure for improved readability and maintainability

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'email-subscription-system/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fb4934e73969295b745a3fdd8e3f44aa809e148b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'email-subscription-system/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fb4934e73969295b745a3fdd8e3f44aa809e148b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.11.1',
+            'version' => '6.11.1.0',
+            'reference' => 'd9e3b36b47f04b497a0164c5a20f92acb4593284',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
