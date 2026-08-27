dbfdb8076bfd38f56cf7f29ea55da5fc49a32ee0
Byron Mukisa <bmukisa13@gmail.com>
2026-08-27 15:39:16 +0300

    ......

--- fln-app/src/app/gallery/page.tsx
+++ fln-app/src/app/gallery/page.tsx
@@ -300,9 +300,9 @@ const galleryImages = [
   { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-25 at 09.54.59 (2).jpeg', title: '', description: "Digital platforms can also amplify women's voices in climate decision-making and create opportunities for women entrepreneurs to develop green businesses.", category: 'workshops' },
   { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-25 at 09.54.59.jpeg', title: '', description: "Invest in women. Equip them with technology. Empower them to lead the climate transition.", category: 'workshops' },
 
-  { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-27 at 15.06.40.jpeg', title: '', description: "Invest in women. Equip them with technology. Empower them to lead the climate transition.", category: 'workshops' },
-  { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-27 at 15.06.40 (1).jpeg', title: '', description: "Invest in women. Equip them with technology. Empower them to lead the climate transition.", category: 'workshops' },
-  { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-27 at 15.06.40 (2).jpeg', title: '', description: "Invest in women. Equip them with technology. Empower them to lead the climate transition.", category: 'workshops' },
+  { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-27 at 15.06.40.jpeg', title: 'Developing climate resilience and adaptability', description: "FLN's Gender Advisor Lavendar  Nassanga training girls on how to develop climate resilience and adaptability in a fast changing world", category: 'workshops' },
+  { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-27 at 15.06.40 (1).jpeg', title: '', description: "", category: 'workshops' },
+  { src: 'imgs/FlnUpdate25-8-26/WhatsApp Image 2026-08-27 at 15.06.40 (2).jpeg', title: '', description: "", category: 'workshops' },
 
 ];
 
